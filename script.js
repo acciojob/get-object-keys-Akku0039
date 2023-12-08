d1a21a39f0ed0751ab7b19218cefc5fcba78b86d
Akku0039 <117562663+Akku0039@users.noreply.github.com>
2023-12-08 12:42:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const student = {
+    name: "John"
+};
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
+
+// Example usage:
+const keys = student.getKeys();
+console.log(keys);
